3b5d3ba23ee8b4289059cde70eaef5442f50c18b
Ahab <habbart01@gmail.com>
2024-07-12 14:29:35 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("BBL Flappy");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BBL_Flappy flappy = new BBL_Flappy();
+        frame.add(flappy);
+        frame.pack();
+        flappy.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
